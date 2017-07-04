dc215bebb79361aadf54e4efa2aaa87fb89a2cd9
sihhan <thehappyend95@gmail.com>
2017-07-04 23:49:05 +0800

    admin can see resume list

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,7 +1,7 @@
 class Resume < ApplicationRecord
 validates :content, presence: true
 
-mount_uploader :attachment, AttachmnetUploader
+mount_uploader :attachment, AttachmentUploader
 
 belongs_to :user
 belongs_to :job
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   resources :jobs do
     resources :resumes
   end
-  
+
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
